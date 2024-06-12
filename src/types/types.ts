001b4781ac6152663da8bf35303353e3de4d68c6
Amir Taheri <amir.taheri.web@protonmail.com>
2024-06-12 12:45:54 +0330

    chore: fake data file added

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,15 @@
 export type TRootLayoutProps = Readonly<{
   children: React.ReactNode;
 }>;
+
+export interface IAnimeProps {
+  id: string;
+  name: string;
+  image: {
+    original: string;
+  };
+  kind: string;
+  episodes: number;
+  episodes_aired: number;
+  score: string;
+}
