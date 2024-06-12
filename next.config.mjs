4bbe42b48eae7730905faf2cf7ffa6f89387e7a9
Amir Taheri <amir.taheri.web@protonmail.com>
2024-06-12 22:39:54 +0330

    fix: next config file updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["shikimori.one"],
+    remotePatterns: [{ protocol: "https", hostname: "shikimori.one" }],
   },
 };
 
